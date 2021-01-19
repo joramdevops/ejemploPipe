3687de96f2487a9bdf8e8256afb1c7d25adc6544
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-19 20:54:16 -0300

    Update Diezxciento.java

--- src/main/java/ejemplopipeline/Diezxciento.java
+++ src/main/java/ejemplopipeline/Diezxciento.java
@@ -4,10 +4,17 @@ public class Diezxciento {
 	
 	public int ahorro;
 	public int sueldo;
+	public int ciframinima;
+	public int tramo1;
+
+	
 	
 	public Diezxciento(){
-		ahorro = 20000000;
-		sueldo = 700000;
+		ahorro = 0;
+		sueldo = 0;
+		ciframinima = 1000000;
+		tramo1 = 10000000;
+		tramo2 = 43000000;
 	}
 	
 	public Diezxciento(int ahorro, int sueldo) {
@@ -16,12 +23,30 @@ public class Diezxciento {
 	}
 	
 	public int diezxciento(){
-		double diezxciento = ahorro * 0.1;
-		System.out.println(diezxciento);
+		double diezxciento;
+		if (ahorro<=ciframinima)
+			diezxciento = ahorro;
+		else if (ahorro > ciframinima && ahorro <= tramo1 )
+			diezxciento =ciframinima;
+		else if (ahorro > tramo1 && ahorro <= tramo2)
+			diezxciento = ahorro * 0.1;
+		else 
+			diezxciento =  tramo2 * 0.1;	
+
 		return (int) diezxciento;
 	}
 	
 	public int impuesto(){
+	/*
+Tramo de remuneración: $0 - $500.000 | Retiro promedio de la persona afiliada: $989.413 | Impuesto a cancelar: $0
+TR: $500.000 - $1.000.000 | RP $1.490.241 | IMP. $0
+TR: $1.00.000 - $1.500.000 | RP: $1.989.013 | IMP: $0
+TR: $1.500.000 - $2.500.000 | RP: $2.247.661 | IMP: $174.531
+TR: $2.500.000 - 3.000.000 | RP: $3.815.984 | IMP: $364.086
+TR: $3.000.000 - $4.000.000 | RP: $3.815.984 | IMP: $612.396
+TR: $4.000.000 - $5.000.000 | RP: $3.815.984 | IMP: 980.357
+TR: $5.000.000 - $6.000.000 | RP: $3.815.984 | IMP: 1.294.052
+TR: $6.000.000 y más | RP: $3.815.984 | IMP: $1.491.042*/ 	
 		return 0;
 	}
 	
