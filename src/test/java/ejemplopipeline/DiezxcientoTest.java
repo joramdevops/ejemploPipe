a656e47aebb27053e34ff4de073ad7af08a4c274
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-19 20:13:02 -0300

    Update DiezxcientoTest.java

--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -15,13 +15,13 @@ public class DiezxcientoTest {
 	@Test
 	public void testImpuesto() throws Exception {
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
-		assertEquals(2400000, diezxciento.impuesto());
+		assertEquals(0, diezxciento.impuesto());
 	}
 
 	@Test
 	public void testSaldoAhorro() throws Exception {
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
-		assertEquals(2400000, diezxciento.saldoAhorro());
+		assertEquals(20700000, diezxciento.saldoAhorro());
 	}
 
 }
